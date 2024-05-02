dc91d7670132e025754a00b3e26ad60f18e2fcad
ahmedramy <ahmedramy.e404@gmail.com>
2024-05-03 00:39:43 +0300

    feat: Finished Login page

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,29 @@
+import {Component, Input} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {NgIf, NgOptimizedImage} from "@angular/common";
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatCardModule, MatFormFieldModule, NgIf, ReactiveFormsModule, MatInput, MatButton, NgOptimizedImage],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  submit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
+
+  @Input() error: string | undefined;
+}
