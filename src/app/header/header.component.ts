737c60cc70d73a0435638ee8b99a9adbaf62f82d
ahmedramy <ahmedramy.e404@gmail.com>
2024-05-04 16:00:41 +0300

    Added navigation in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {RouterLink} from "@angular/router";
+import {MatButton} from "@angular/material/button";
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, RouterLink, MatButton],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
